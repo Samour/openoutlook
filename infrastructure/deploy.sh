d0a3a40328038fdef92887dc2e7cb7cac647e2c9
Aaron Burke <aaron.burke@swipejobs.com>
2020-07-23 12:19:48 +1000

    #10 Update FE promote script to retain env-specific /public files

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -40,13 +40,21 @@ fetch-db() {
 promote() {
   ssh $REMOTE_ADDR <<<"""
   systemctl stop strapi-$PRODUCTION
-  cp -r /var/www/$STAGING/fe /var/www/$PRODUCTION/fe
+
+  cp -r /var/www/$STAGING/fe /var/www/$PRODUCTION/fe_new
+  cp -r /var/www/$PRODUCTION/fe/public/* /var/www/$PRODUCTION/fe_new/public/
+  cp /var/www/$STAGING/fe/public/index.html /var/www/$PRODUCTION/fe_new/public/
+  rm -r /var/www/$PRODUCTION/fe
+  mv /var/www/$PRODUCTION/fe_new /var/www/$PRODUCTION/fe
+
   BAK_FNAME=/var/www/$PRODUCTION/strapi/.tmp/data.db.\`date -Is\`
   cp /var/www/$PRODUCTION/strapi/.tmp/data.db \$BAK_FNAME
   cp /var/www/$STAGING/strapi/.tmp/data.db /var/www/$PRODUCTION/strapi/.tmp/data.db
   cp -r /var/www/$STAGING/strapi/public/uploads/* /var/www/$PRODUCTION/strapi/public/uploads/
+
   sqlite /var/www/$PRODUCTION/strapi/.tmp/data.db \"DROP TABLE IF EXISTS $ENQUIRIES_TABLE\"
   sqlite \$BAK_FNAME \".dump $ENQUIRIES_TABLE\" | sqlite /var/www/$PRODUCTION/strapi/.tmp/data.db
+
   systemctl start strapi-$PRODUCTION
   """
 }
