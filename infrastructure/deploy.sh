876375032e656709ce3d219b6f4639d83bd09587
Aaron Burke <57202540+aaronb-swipejobs@users.noreply.github.com>
2020-08-15 21:35:22 +1000

    #10 Script to promote website from staging to prod (#16)
    * #10 Script to promote website from staging to prod
    
    * #10 Update FE promote script to retain env-specific /public files
    
    * #10 Slight improvement to service availability during promotion
    
    * Minor fix for script
    
    * Fix problem with promote script
    
    Co-authored-by: Aaron <aaron@Edvals-MacBook-Air.local>

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -37,6 +37,28 @@ fetch-db() {
   rsync -vz $REMOTE_ADDR:/var/www/$ENV/strapi/public/uploads/* ../strapi/public/uploads/
 }
 
+promote() {
+  ssh $REMOTE_ADDR <<<"""
+  cp -r /var/www/$STAGING/fe /var/www/$PRODUCTION/fe_new
+  cp -r /var/www/$PRODUCTION/fe/* /var/www/$PRODUCTION/fe_new/
+  cp /var/www/$STAGING/fe/index.html /var/www/$PRODUCTION/fe_new/
+  rm -r /var/www/$PRODUCTION/fe
+  mv /var/www/$PRODUCTION/fe_new /var/www/$PRODUCTION/fe
+
+  systemctl stop strapi-$PRODUCTION
+
+  BAK_FNAME=/var/www/$PRODUCTION/strapi/.tmp/data.db.\`date -Is\`
+  cp /var/www/$PRODUCTION/strapi/.tmp/data.db \$BAK_FNAME
+  cp /var/www/$STAGING/strapi/.tmp/data.db /var/www/$PRODUCTION/strapi/.tmp/data.db
+  cp -r /var/www/$STAGING/strapi/public/uploads/* /var/www/$PRODUCTION/strapi/public/uploads/
+
+  sqlite3 /var/www/$PRODUCTION/strapi/.tmp/data.db \"DROP TABLE IF EXISTS $ENQUIRIES_TABLE\"
+  sqlite3 \$BAK_FNAME \".dump $ENQUIRIES_TABLE\" | sqlite3 /var/www/$PRODUCTION/strapi/.tmp/data.db
+
+  systemctl start strapi-$PRODUCTION
+  """
+}
+
 if [ "$1" == "fe" ]; then
   deploy-fe
 elif [ "$1" == "be" ]; then
@@ -49,6 +71,8 @@ elif [ "$1" == "all" ]; then
   deploy-fe
 elif [ "$1" == "fetch-db" ]; then
   fetch-db
+elif [ "$1" == "promote" ]; then
+  promote
 else
   echo "System not specified. Specify fe or be"
 fi
